dbd313325892f6aa0d1e0166d6d128f10a35c20b
Igor Veyner <igorveyner95@gmail.com>
2021-07-28 06:47:29 -0400

    add router and nav stub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Nav from './components/Header/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+
+      <Switch>
+        <Route path="/">
+          {/* add landing page */}
+        </Route>
+        <Route path="/play">
+          {/* add gameboard component */}
+        </Route>
+        <Route exact path="/about">
+          {/* add about component */}
+        </Route>
+        <Route path="*">
+          {/* add fallback */}
+        </Route>
+      </Switch>
+    </ Router>
   );
 }
 
---
+++ src/components/Header/Nav/index.js
@@ -0,0 +1,7 @@
+const Nav = () => {
+  return (
+    <p>test</p>
+  )
+}
+
+export default Nav
