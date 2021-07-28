e9f2d6a0fb00a19c1a6a543bfbb5ec5cb626bd09
Igor Veyner <igorveyner95@gmail.com>
2021-07-28 06:57:36 -0400

    add sass and update nav

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+
 const Nav = () => {
   return (
-    <p>test</p>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/play">Play</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
