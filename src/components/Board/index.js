b8bb6d62539afcdf1b295315e0feb526488da5c4
Igor Veyner <igorveyner95@gmail.com>
2021-07-28 10:36:16 -0400

    add pregame component

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { setPlayerHand, setComputerHand } from '../../redux/actions/handActions'
 import { getHands } from "../../services/hands"
+import PreGameLobby from './pregame'
 
 const Board = () => {
   const dispatch = useDispatch()
   const playerHand = useSelector(state => state.player)
   const computerHand = useSelector(state => state.computer)
+
+  const playerCards = playerHand['hand'].length
+  const computerCards = computerHand['hand'].length
 
   const startGame = () => {
     const hands = getHands()
@@ -15,15 +19,16 @@ const Board = () => {
 
   return (
     <div>
-      Playing the game
-      <button onClick={startGame}>
-        Play
-      </button>
+      { playerCards === 0 && computerCards === 0 ? 
+        <PreGameLobby startGame={startGame} /> 
+        :
+        null  
+      }
       <div>
-        Player has {playerHand['hand'].length} cards
+        Player has {playerCards} cards
       </div>
       <div>
-        Computer has {computerHand['hand'].length} cards
+        Computer has {computerCards} cards
       </div>
     </div>
   )
---
+++ src/components/Board/pregame/index.js
@@ -0,0 +1,8 @@
+const PreGameLobby = ({ startGame }) => {
+  return (
+  <button onClick={startGame}>
+    Deal Cards
+  </button>)
+}
+
+export default PreGameLobby
