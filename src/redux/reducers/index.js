c878c35274e2fd1757dbf1679ce846df2d1f1276
Igor Veyner <igorveyner95@gmail.com>
2021-07-28 09:16:51 -0400

    add reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux"
+import handleComputer from "./computerReducer"
+import handlePlayer from "./playerReducer"
 
 const rootReducer = combineReducers({
-  
+  player: handlePlayer,
+  computer: handleComputer
 })
 
 export default rootReducer
---
+++ src/redux/reducers/playerReducer.js
@@ -0,0 +1,21 @@
+const handlePlayer = (state = {
+  hand: []
+                      }, action) => {
+  switch (action.type) {
+    case "RESET_HAND":
+      return { hand: [] }
+
+    case "SET_HAND":
+      // set the new hand
+      return action.payload
+
+    case "REMOVE_CARD":
+      // remove from hand the card from action.payload
+      return action.payload
+
+    default: 
+      return state
+  }
+}
+
+export default handlePlayer
