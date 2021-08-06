1b9c0cb1a4758723b9204c8b0a609a6d935e09a4
Igor Veyner <igorveyner95@gmail.com>
2021-08-06 08:54:55 -0400

    render last played cards on screen

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux"
+import { urls } from "./urlLookup"
 
 const CardsContainer = ({ gameStatus }) => {
   const allCards = useSelector(state => state.ledger),
@@ -8,16 +9,41 @@ const CardsContainer = ({ gameStatus }) => {
   const renderCards = () => {
     if (gameStatus && allCards.length > 0) {
       // render the cards
-      const playerCard = allCards[0]['player'],
-        computerCard = allCards[0]['computer']
+      const playerCard = Object.values(allCards[0]['player']).join("-of-"),
+        computerCard = Object.values(allCards[0]['computer']).join("-of-"),
+        cardInfo = [
+          ["Player", urls[playerCard], playerCard],
+          ["Computer", urls[computerCard], computerCard]
+        ]
+      
+      return cardInfo.map((data, i) => {
+        return renderContainer(data)
+      })
 
     } else {
       // render empty board
+      const cardInfo = [
+        ["Player", urls["back"], "Card-Back"],
+        ["Computer", urls["back"], "Card-Back"]
+      ]
+
+      return cardInfo.map((data, i) => {
+        return renderContainer(data)
+      })
     }
   }
 
+  const renderContainer = ([ player, url, text ]) => {
+    return (
+      <div>
+        <h1>{player}</h1>
+        <img src={url} alt={text} />
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <div className="cards">
       {renderCards()}
     </div>
   )
