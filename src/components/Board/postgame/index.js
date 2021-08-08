85f48d60c60bcdb70c7eef100da5ce42ae0e4ea6
Igor Veyner <igorveyner95@gmail.com>
2021-08-07 20:13:41 -0400

    add play again button and functionality

--- src/components/Board/postgame/index.js
+++ src/components/Board/postgame/index.js
@@ -1,7 +1,12 @@
 const PostGame = ({ winner }) => {
+  const refreshPage = () => {
+    window.location.reload(false)
+  }
+  
   return (
     <div>
       <h2>{winner}</h2>
+      <button onClick={refreshPage}>Play Again</button>
     </div>
   )
 }
