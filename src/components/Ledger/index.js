4195c4075ca861075824986a70e4965e2b94b6b9
Igor Veyner <igorveyner95@gmail.com>
2021-08-11 09:17:35 -0400

    remove old code

--- src/components/Ledger/index.js
+++ src/components/Ledger/index.js
@@ -3,18 +3,6 @@ import { useSelector } from "react-redux"
 const Ledger = () => {
   const ledger = useSelector(state => state.ledger)
 
-  // const renderHistory = () => {
-  //   return ledger.map((pair, i) => {
-  //     return (
-  //       <div key={`card-pair-${i}`}>
-  //         {`Player: ${pair['player'].rank} of ${pair['player'].suit} 
-  //           vs
-  //           Computer: ${pair['computer'].rank} of ${pair['computer'].suit}`}
-  //       </div>
-  //     )
-  //   })
-  // }
-
   const renderHistory = () => {
     return ledger.map((pair, i) => {
       return (
